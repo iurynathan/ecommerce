5fd75b4fb46af486253acf0860664af5aff714f7
Iury Nathan <iurynathan@gmail.com>
2023-04-05 15:33:25 -0300

    ajuste PORT

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const createServer = require('./server');
 require('dotenv').config();
 
 const app = createServer();
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server has started on PORT: ${PORT}`);
