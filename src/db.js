2d2d473a75adf6135f3aa3d60493d7de096d77a4
Iury Nathan <iurynathan@gmail.com>
2023-04-05 15:58:34 -0300

    try catch conexão mongodb

--- src/db.js
+++ src/db.js
@@ -1,18 +1,23 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-const MONGO_DB_URL = 'mongodb://root:root@172.19.0.2:27017/ecommerce';
-const DB_NAME = 'ecommerce';
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
+const DB_NAME = process.env.DB_NAME;
 
 let db;
 let client;
 
 async function connect(mongoUri) {
+  try {
   if (!db) {
     const uriToUse = mongoUri || MONGO_DB_URL;
     client = await MongoClient.connect(uriToUse, { useNewUrlParser: true, useUnifiedTopology: true });
     db = client.db(DB_NAME);
   }
   return db;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function disconnect() {
