14b9c53707343781d0ddb47acedb5b0ff1b92c1b
Iury Nathan <iurynathan@gmail.com>
2023-04-06 06:09:57 -0300

    adicionado cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const categoriesRouter = require('./routes/categoryRoute');
 const productsRouter = require('./routes/productsRoute');
 
@@ -9,6 +10,7 @@ function createServer() {
     res.send('Response OK');
   });
 
+  app.use(cors());
   app.use(express.json());
 
   app.use('/categories', categoriesRouter);
